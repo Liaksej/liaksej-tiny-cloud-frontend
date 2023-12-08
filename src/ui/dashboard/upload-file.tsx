e81121a85e73177e64fc68a82dbb0b9f5667f4ba
Liaksej <tarusov.liaksej@gmail.com>
2023-12-08 14:06:05 +0100

    Implement file sending server function, remove unused icons and fields
    The commit implements the "sendFileToServer" function within the formation data sent for file upload. Unnecessary code, such as the `AtSymbolIcon` import and error handling-related HTML, has been removed. The password field is no longer marked as required, enhancing the user experience.

--- src/ui/dashboard/upload-file.tsx
+++ src/ui/dashboard/upload-file.tsx
@@ -2,7 +2,6 @@
 
 import {
   ArrowUpTrayIcon,
-  AtSymbolIcon,
   DocumentIcon,
   ExclamationCircleIcon,
   KeyIcon,
@@ -44,6 +43,7 @@ export default function UploadFile() {
     if (file) {
       formData.append("file", file);
       formData.append("file_name", file.name);
+      await sendFileToServer(formData);
       setIsModalOpen(false);
       setFile(undefined);
     }
@@ -98,7 +98,6 @@ export default function UploadFile() {
                         name="password"
                         placeholder="Enter comment..."
                         rows={3}
-                        required
                         minLength={6}
                       />
                       {/*<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
@@ -109,16 +108,7 @@ export default function UploadFile() {
                 <div onClick={handleClose}>
                   <CancelButton />
                 </div>
-                {/*<div className="flex h-8 items-end space-x-1">*/}
-                {/*  {Boolean(errorMessage) && (*/}
-                {/*    <>*/}
-                {/*      <ExclamationCircleIcon className="h-5 w-5 text-red-500" />*/}
-                {/*      <p aria-live="polite" className="text-sm text-red-500">*/}
-                {/*        {errorMessage}*/}
-                {/*      </p>*/}
-                {/*    </>*/}
-                {/*  )}*/}
-                {/*</div>*/}
+                <div className="flex h-8 items-end space-x-1"></div>
               </div>
             </form>
           </ModalUpload>,
