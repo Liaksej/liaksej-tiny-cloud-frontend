ba8a43c1c6af2fe4259c92ca3e1a9f7be56a2cfd
Liaksej <tarusov.liaksej@gmail.com>
2023-12-16 15:41:29 +0100

    Refactor download route and remove related config
    This commit removes the download routing configuration from `next.config.js` and creates a dedicated file `route.ts` for handling downloads. The new file uses asynchronous functions to check for parameters, authenticate the session, and fetch data from the backend, therefore improving the application’s routing structure and request handling.

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
     return {
       afterFiles: [
         {
-          source: "/download/:path*",
-          destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}cloud/download/:path*/`,
-        },
-        {
           source: "/public/:path*",
           destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}cloud/public/:path*/`,
         },
---
+++ src/app/download/[id]/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@/auth.config";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  if (!params || !params.id) {
+    throw new Error(`Parameters are required`);
+  }
+  const session = await auth();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}cloud/download/${params.id}/`,
+    {
+      headers: {
+        Authorization: `Bearer ${session?.user?.access}`,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(`Response failed with status ${response.status}`);
+  }
+
+  const blob = await response.blob();
+
+  return new Response(blob);
+}
