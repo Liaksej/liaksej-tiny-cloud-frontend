b5f77965cc43b1d80f5af6409a2458184febbac1
Liaksej <tarusov.liaksej@gmail.com>
2023-12-17 19:47:23 +0100

    Refactor data fetching in data.ts
    Centralized and extracted data fetching logic into a standalone function named fetchDataFromAPI. This includes error handling, adding query params and handling authorization. This now makes the data fetching code more DRY (do not repeat yourself) for use across multiple other functions.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,28 +1,55 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { auth } from "@/auth.config";
 
-// export async function fetchLatestInvoices() {
-//   noStore();
-//   try {
-//     const data = await sql<LatestInvoiceRaw>`
-//       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-//       FROM invoices
-//       JOIN customers ON invoices.customer_id = customers.id
-//       ORDER BY invoices.date DESC
-//       LIMIT 5`;
-//
-//     const latestInvoices = data.rows.map((invoice) => ({
-//       ...invoice,
-//       amount: formatCurrency(invoice.amount),
-//     }));
-//     return latestInvoices;
-//   } catch (error) {
-//     console.error("Database Error:", error);
-//     throw new Error("Failed to fetch the latest invoices.");
-//   }
-// }
-
 const ITEMS_PER_PAGE = 10;
+
+async function fetchDataFromAPI(
+  endpoint: string,
+  queryParams?: Record<string, any>,
+  method = "GET",
+) {
+  noStore();
+  const session = await auth();
+  const url = new URL(`${process.env.NEXTAUTH_BACKEND_URL}${endpoint}`);
+
+  if (queryParams) {
+    Object.entries(queryParams).forEach(([key, value]) => {
+      if (value !== null && value !== undefined) {
+        url.searchParams.append(key, value.toString());
+      }
+    });
+  }
+
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        ContentType: "application/json",
+        Authorization: `Bearer ${session?.user?.access}`,
+      },
+    });
+
+    if (
+      response.status === 404 &&
+      (endpoint === "auth/users/" || endpoint === "cloud/files/")
+    ) {
+      const info = await response.json();
+      if (info.detail == "Invalid page.") {
+        return;
+      }
+    }
+
+    if (!response.ok) {
+      console.error("Fetch Error:", response.statusText);
+      throw new Error(`Failed to fetch ${endpoint}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    throw new Error(`Failed to fetch ${endpoint}`);
+  }
+}
 
 export async function fetchTableData(
   query: string,
@@ -30,49 +57,10 @@ export async function fetchTableData(
   type: "files" | "users",
   name?: string,
 ) {
-  noStore();
-  const session = await auth();
-
-  const params = new Map<string, string | number | null>([
-    ["username", name ? name : null],
-    ["page", currentPage > 1 ? currentPage : null],
-    ["q", query ? query : null],
-  ]);
-
-  const url = new URL(
-    `${process.env.NEXTAUTH_BACKEND_URL}${
-      type === "users" ? "auth/users/" : "cloud/files/"
-    }`,
+  return await fetchDataFromAPI(
+    `${type === "users" ? "auth/users/" : "cloud/files/"}`,
+    { username: name, page: currentPage > 1 ? currentPage : null, q: query },
   );
-
-  params.forEach((value, key) => {
-    if (value === null) return;
-
-    url.searchParams.append(key, value.toString());
-  });
-
-  try {
-    const response = await fetch(url, {
-      headers: {
-        ContentType: "application/json",
-        Authorization: `Bearer ${session?.user?.access}`,
-      },
-    });
-    if (response.status === 404) {
-      const info = await response.json();
-      if (info.detail == "Invalid page.") {
-        return;
-      }
-    }
-    if (!response.ok) {
-      throw new Error("Failed to fetch table data.");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Fetch Error:", error);
-    throw new Error("Failed to fetch table data.");
-  }
 }
 
 export async function fetchFilesPages(
@@ -80,156 +68,19 @@ export async function fetchFilesPages(
   type: "files" | "users",
   name?: string,
 ) {
-  noStore();
-  const session = await auth();
-
-  const params = new Map<string, string | number | null>([
-    ["username", name ? name : null],
-    ["q", query ? query : null],
-  ]);
-
-  const url = new URL(
-    `${process.env.NEXTAUTH_BACKEND_URL}${
-      type === "users" ? "auth/users/" : "cloud/files/"
-    }`,
+  const data = await fetchDataFromAPI(
+    `${type === "users" ? "auth/users/" : "cloud/files/"}`,
+    { username: name, q: query },
   );
-
-  params.forEach((value, key) => {
-    if (value === null) return;
-
-    url.searchParams.append(key, value.toString());
-  });
-
-  try {
-    const countResponse = await fetch(url, {
-      headers: {
-        ContentType: "application/json",
-        Authorization: `Bearer ${session?.user?.access}`,
-      },
-    });
-
-    if (!countResponse.ok) {
-      throw new Error("Failed to fetch invoices.");
-    }
-
-    const { count } = await countResponse.json();
-
-    return Math.ceil(Number(count) / ITEMS_PER_PAGE);
-  } catch (error) {
-    console.error("Fetch Error:", error);
-    throw new Error("Failed to fetch total number of files!!!!!.");
-  }
+  return Math.ceil(data.count / ITEMS_PER_PAGE);
 }
 
 export async function fetchFile(id: string) {
-  noStore();
-  const session = await auth();
-  try {
-    const countResponse = await fetch(
-      `${process.env.NEXTAUTH_BACKEND_URL}cloud/files/${id}`,
-      {
-        headers: {
-          ContentType: "application/json",
-          Authorization: `Bearer ${session?.user?.access}`,
-        },
-      },
-    );
-
-    if (!countResponse.ok) {
-      throw new Error("Failed to fetch file.");
-    }
-
-    return await countResponse.json();
-  } catch (error) {
-    console.error("Fetch Error:", error);
-  }
+  return await fetchDataFromAPI(`cloud/files/${id}`);
 }
 
 export async function adminCheck() {
-  noStore();
-
   const session = await auth();
-
-  try {
-    const response = await fetch(
-      `${process.env.NEXTAUTH_BACKEND_URL}auth/users/${session?.user?.name}/`,
-      {
-        headers: {
-          ContentType: "application/json",
-          Authorization: `Bearer ${session?.user?.access}`,
-        },
-      },
-    );
-    if (response.ok) {
-      const data = await response.json();
-      return !!data.is_staff;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.error("Fetch Error:", error);
-  }
+  const data = await fetchDataFromAPI(`auth/users/${session?.user?.name}/`);
+  return !!data.is_staff;
 }
-
-// export async function fetchCustomers() {
-//   noStore();
-//   try {
-//     const data = await sql<CustomerField>`
-//       SELECT
-//         id,
-//         name
-//       FROM customers
-//       ORDER BY name ASC
-//     `;
-//
-//     const customers = data.rows;
-//     return customers;
-//   } catch (err) {
-//     console.error("Database Error:", err);
-//     throw new Error("Failed to fetch all customers.");
-//   }
-// }
-
-// export async function fetchFilteredCustomers(query: string) {
-//   noStore();
-//   try {
-//     const data = await sql<CustomersTable>`
-// 		SELECT
-// 		  customers.id,
-// 		  customers.name,
-// 		  customers.email,
-// 		  customers.image_url,
-// 		  COUNT(invoices.id) AS total_invoices,
-// 		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-// 		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-// 		FROM customers
-// 		LEFT JOIN invoices ON customers.id = invoices.customer_id
-// 		WHERE
-// 		  customers.name ILIKE ${`%${query}%`} OR
-//         customers.email ILIKE ${`%${query}%`}
-// 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-// 		ORDER BY customers.name ASC
-// 	  `;
-//
-//     const customers = data.rows.map((customer) => ({
-//       ...customer,
-//       total_pending: formatCurrency(customer.total_pending),
-//       total_paid: formatCurrency(customer.total_paid),
-//     }));
-//
-//     return customers;
-//   } catch (err) {
-//     console.error("Database Error:", err);
-//     throw new Error("Failed to fetch customer table.");
-//   }
-// }
-
-// export async function getUser(email: string) {
-//   try {
-//     const user = await sql`SELECT * from USERS where email=${email}`;
-//     return user.rows[0] as User;
-//   } catch (error) {
-//     console.error("Failed to fetch user:", error);
-//     throw new Error("Failed to fetch user.");
-//   }
-// }
