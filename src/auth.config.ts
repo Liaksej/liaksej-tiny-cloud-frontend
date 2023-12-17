c7fd1b64b9aa6e31a563c79e5aa7de5a097c919c
Liaksej <tarusov.liaksej@gmail.com>
2023-12-17 19:47:34 +0100

    Refactor auth code and optimize routing checks
    Centralized data fetching to a standalone `fetchDataFromBackend` function and refactored token refresh logic in the auth file. Optimized route checking using a switch statement in the authorized function for improved readability and maintainability.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -8,30 +8,31 @@ import { adminCheck } from "@/lib/data";
 const BACKEND_ACCESS_TOKEN_LIFETIME = 45 * 60; // 45 minutes
 const BACKEND_REFRESH_TOKEN_LIFETIME = 7 * 24 * 60 * 60; // 7 days
 
-async function getUser(email: string, password: string) {
+async function fetchAuthDataFromBackend(
+  endpoint: string,
+  body: { email: string; password: string } | { refresh: string },
+  method: "POST" | "GET" = "POST",
+) {
   const response = await fetch(
-    `${process.env.NEXTAUTH_BACKEND_URL}auth/login/`,
+    `${process.env.NEXTAUTH_BACKEND_URL}${endpoint}`,
     {
-      method: "POST",
+      method,
       headers: {
         "Content-Type": "application/json",
       },
       credentials: "include",
-      body: JSON.stringify({
-        email,
-        password,
-      }),
+      body: JSON.stringify(body),
     },
   );
   if (response.ok) {
     return await response.json();
-  } else {
-    console.error("Error logging in" + response.status);
-    return null;
   }
+
+  console.error("Error with http request: " + response.status);
+  return null;
 }
 
-export function getCurrentEpochTime() {
+function getCurrentEpochTime() {
   return Math.floor(Date.now() / 1000);
 }
 
@@ -53,7 +54,10 @@ export const authConfig = {
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
 
-          const data = await getUser(email, password);
+          const data = await fetchAuthDataFromBackend("auth/login/", {
+            email,
+            password,
+          });
           if (!data) return null;
 
           return {
@@ -90,27 +94,16 @@ export const authConfig = {
         return token;
       }
       if (token.expires && currentEpochTime > Number(token.expires)) {
-        const response = await fetch(
-          `${process.env.NEXTAUTH_BACKEND_URL}auth/token/refresh/`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              refresh: token.refresh,
-            }),
-          },
-        );
-        if (response.ok) {
-          const data = await response.json();
-          token.access = data.access;
-          token.expires = currentEpochTime + BACKEND_ACCESS_TOKEN_LIFETIME;
-          return token;
-        } else {
-          console.log("Error refreshing access token" + response.status);
+        const data = await fetchAuthDataFromBackend("auth/token/refresh/", {
+          refresh: token.refresh as string,
+        });
+        if (!data) {
+          console.log("Error refreshing access token" + data.status);
           return await signOut();
         }
+        token.access = data.access;
+        token.expires = currentEpochTime + BACKEND_ACCESS_TOKEN_LIFETIME;
+        return token;
       }
       if (
         token.refreshExpires &&
@@ -123,34 +116,29 @@ export const authConfig = {
       return token;
     },
     async authorized({ auth, request: { nextUrl } }) {
-      const isSignupPage = nextUrl.pathname.startsWith("/signup");
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      const isOnAdmin = nextUrl.pathname.startsWith("/admin");
-      const isDownload = nextUrl.pathname.startsWith("/download/");
-      const isPublic = nextUrl.pathname.startsWith("/public/");
-      if (isSignupPage) {
-        if (!isLoggedIn) return true;
-        return Response.redirect(new URL("/dashboard", nextUrl));
+      const path = nextUrl.pathname;
+
+      switch (true) {
+        case path.startsWith("/signup"):
+          return isLoggedIn
+            ? Response.redirect(new URL("/dashboard", nextUrl))
+            : true;
+        case path.startsWith("/admin"):
+          return isLoggedIn
+            ? await adminCheck()
+            : Response.redirect(new URL("/login", nextUrl));
+        case path.startsWith("/download"):
+          return isLoggedIn ? true : Response.redirect(new URL("/", nextUrl));
+        case path.startsWith("/public"):
+          return true;
+        case path.startsWith("/dashboard"):
+          return isLoggedIn;
+        default:
+          return isLoggedIn
+            ? Response.redirect(new URL("/dashboard", nextUrl))
+            : true;
       }
-      if (isOnAdmin) {
-        if (isLoggedIn) return adminCheck();
-        return Response.redirect(new URL("/login", nextUrl));
-      }
-      if (isDownload) {
-        if (isLoggedIn) return true;
-        return Response.redirect(new URL("/login", nextUrl));
-      }
-      if (isPublic) {
-        return true;
-      }
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
-      }
-      return true;
     },
   },
 } satisfies NextAuthConfig;
