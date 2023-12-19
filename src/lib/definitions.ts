913a0f4be269393f56fa719eba0dfbb775f83f82
Liaksej <tarusov.liaksej@gmail.com>
2023-12-19 19:35:19 +0100

    Updated README.md with CI/CD information
    The README.md file has been updated with detailed instructions on how to use the improved CI/CD for quick server updates. This includes commands for updating containers on the server and information on the protection of files and the database.

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -22,19 +22,6 @@ export type LoginData = {
   };
 };
 
-export type LatestInvoice = {
-  id: string;
-  name: string;
-  image_url: string;
-  email: string;
-  amount: string;
-};
-
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
-export type LatestInvoiceRaw = Omit<LatestInvoice, "amount"> & {
-  amount: number;
-};
-
 export interface File {
   id: string;
   original_name: string;
